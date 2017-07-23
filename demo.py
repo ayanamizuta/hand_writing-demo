67cce329a78d16ac6ad02435281de79788fe4ab4
Mizuta Rei <mizuta1018@gmail.com>
2017-07-23 18:47:56 +0900

    NN method

--- demo.py
+++ demo.py
@@ -6,34 +6,43 @@ from itertools import product
 class MyHTTPRequestHandler(SocketServer.StreamRequestHandler):
 
     def trim(self,s):
-        h_w = np.zeros((28,28)) 
-        for i,j in product(range(28),range(28)):
-            for k in range(8):
-                h_w[i,j] += sum([float(x) for x in s[(i*8+k)*28:(i*8+k)*28+8]])
+        h_w = np.zeros(784)
+        for i,k in product(range(784),range(8)):
+            r = i / 28
+            c = i % 28
+            h_w[i] += sum([float(x) for x in s[(r*8+k)*224+c*8:(r*8+k)*224+c*8+8]])
         return h_w
 
     def handle(self):
-        data = self.rfile.readline().strip()
-        print data
+        #data = self.rfile.readline().strip()
+        #print data
         
-        if data.find("POST") >= 0:
-            while data.find("write_down=") < 0:
-                data = self.rfile.readline().strip()
-                print data
-
+        #if data.find("POST") >= 0:
+        #    while data.find("write_down=") < 0 or data == "":
+        #        data = self.rfile.readline().strip()
+        #        print data
+        data = ""
+        if self.request.recv(1) != "G":
+            recv = self.request.recv(8192)
+            #print recv
+            recv = self.request.recv(8192)
+            #print recv
+            if recv.find("write_down=") >= 0:
+                data += recv[recv.find("write_down="):-1]
+                data += self.request.recv(224*224+len("write_down=")-len(data))
+                #print data
+        
         f = open("./demo.html", 'r')
         str_pre = f.read()
 
         self.wfile.write("HTTP/1.1 200 OK\r\n")
         self.wfile.write("Content-Type: text/html; charset=utf-8\r\n")
         self.wfile.write("\r\n")
-        if data.find("?write_down=") != 0:
+        if data.find("write_down=") != 0:
             self.wfile.write(str_pre)
         else:
-            d = data.split(' ')[1]
-            _input = trim(d["?write_down=".length:-1])
-            str_pre.replace("browser_demo",str(network.predict(_input)))
-            self.wfile.write(str_pre)
+            _input = self.trim(data[len("write_down="):-1])
+            self.wfile.write(str_pre.replace("browser_demo",str(np.argmax(network.predict(_input)))))
         f.close()
 
 HOST = '127.0.0.1'
