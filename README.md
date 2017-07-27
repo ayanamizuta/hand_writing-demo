275ce4a7f1ceec37cb4d025b325167c16bbbfeae
Mizuta Rei <mizuta1018@gmail.com>
2017-07-27 19:57:45 +0900

    README

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # hand_writing-demo
 a browser demonstration of hand writing recognition
 
+## Dependencies
+python 2.7.10
+numpy 1.13.0
+
+## Run
+
 ```bash
 python train.py
 python demo.py
--- train.py
+++ train.py
@@ -75,7 +75,7 @@ f.close()
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
 
-max_epochs = 10
+max_epochs = 20
 
 network = SimpleConvNet(input_dim=(1,28,28), 
                         conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
